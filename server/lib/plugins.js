351bbbdfbf2b19306c86f221fdacd1199bab1987
Adam Barnhard <admbarn@gmail.com>
2014-12-16 20:23:52 -0600

    adding server & route config templates

---
+++ server/lib/plugins.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = [
+  {
+    register: require('good'),
+    options: {
+      reporters: [{
+        reporter: require('good-console'),
+        args: [{log: '*', response: '*', error: '*'}]
+      }]
+    }
+  },
+  {
+    register: require('lout')
+  },
+  {
+    register: require('hapi-auth-cookie')
+  }
+];
