a2f3b88fa4638dd700ff7ad308d773d145501f3d
Adam Barnhard <admbarn@gmail.com>
2015-01-03 16:52:17 -0600

    added scheduler function to front end

--- client/views/schedule/schedule.js
+++ client/views/schedule/schedule.js
@@ -2,8 +2,9 @@
   'use strict';
 
   angular.module('trainer')
-  .controller('ScheduleCtrl', ['$scope', 'Schedule', function($scope, Schedule){
-    $scope.workouts = [
+  .controller('ScheduleCtrl', ['$scope', 'Schedule', 'Workout', function($scope, Schedule, Workout){
+    // initialize variables on scope
+    $scope.liftDays = [
       {
         id: 1,
         title: 'All Day Event',
@@ -11,19 +12,100 @@
         allDay: true
       }
     ];
+    $scope.regimes         = [];
+    $scope.phases          = [];
+    $scope.workouts        = [];
+    $scope.regime          = null;
+    $scope.phase           = null;
+    $scope.selectedWorkout = null;
 
-    function clickHandler(obj){
-      if(obj.title){
-        console.log('event click', obj.id, obj.start.format());
-      }else{
-        console.log('day click', obj.format(), $scope.workouts[0].start, obj.format() === $scope.workouts[0].start);
-        if(false){
-          console.log('Day has event scheduled');
-        }
-      }
-      // console.log(isValidDate(obj), Object.prototype.toString.call(obj), obj.title);
+    // define querys for workouts modals
+    function queryRegimes(){
+      Workout.getRegimes().then(function(res){
+        $scope.regimes = res.data.regimes;
+      });
     }
 
+    function queryPhases(regimeId){
+      Workout.getPhases(regimeId).then(function(res){
+        $scope.phases = res.data.phases;
+      });
+    }
+
+    function queryWorkouts(phaseId){
+      Workout.getWorkouts(phaseId).then(function(res){
+        $scope.workouts = res.data.workouts;
+      });
+    }
+
+    // selector function for adding a workout to the schedule
+    $scope.selectWorkout = function(workout){
+      $scope.selectedWorkout = workout;
+    };
+
+    // define functions to handle clicking on a calendar day/event
+    function hasEventScheduled(day, events){
+      var scheduled = events.some(function(e){
+        return e.start === day.format();
+      });
+      return scheduled;
+    }
+
+    function findEventIndex(day, events){
+      return events.map(function(e){return e.start;}).indexOf(day.format());
+    }
+
+    function showModal(modalId){
+      $(modalId).foundation('reveal', 'open');
+    }
+
+    function editEvent(obj){
+      var index = findEventIndex(obj.start, $scope.workouts);
+      $scope.$apply(function(){$scope.selectedWorkout = $scope.liftDays[index];});
+      showModal('#editModal');
+    }
+
+    $scope.clickHandler = function(obj){
+      if(obj.title){
+        editEvent(obj);
+      }else{
+        if(hasEventScheduled(obj, $scope.workouts)){
+          editEvent(obj);
+        }else{
+          $scope.$apply(function(){$scope.selectedDate = obj.format();});
+          showModal('#scheduleModal');
+        }
+      }
+    };
+
+    // get all regimes for the user
+    queryRegimes();
+
+    // set event listeners to clear vars and query workouts/phases
+    angular.element(document).on('closed.fndtn.reveal', function(e){
+      // console.log('modal closed');
+      $scope.selectedWorkout = $scope.selectedDate = $scope.selectedWorkout = null;
+    });
+
+    $scope.$watch('regime', function(selectedRegime, oldVal){
+      // console.log(selectedRegime, oldVal);
+      if(selectedRegime){
+        queryPhases(selectedRegime.id);
+      }else{
+        $scope.phases = [];
+      }
+    });
+
+    $scope.$watch('phase', function(selectedPhase, oldVal){
+      // console.log(selectedPhase, oldVal);
+      if(selectedPhase){
+        queryWorkouts(selectedPhase.id);
+      }else{
+        $scope.workouts = [];
+      }
+    });
+
+    // initalize and load calendar widget
     angular.element(document).ready(function(){
       angular.element('#calendar').fullCalendar({
         header: {
@@ -31,13 +113,12 @@
           center: 'title',
           right: 'prev,next'
         },
+        height: 450,
         editable: false,
         eventLimit: true,
-        events: $scope.workouts,
-        eventClick: clickHandler,
-        dayClick: clickHandler
-        // eventClick: function(calEvent){console.log('event click',calEvent, calEvent.start.format());},
-        // dayClick: function(date){console.log('day click',date.format());},
+        events: $scope.liftDays,
+        eventClick: $scope.clickHandler,
+        dayClick: $scope.clickHandler
       });
     });
   }]);
