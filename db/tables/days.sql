c62608d9d838e17a62b24d56526ad4435d0a397a
Adam Barnhard <admbarn@gmail.com>
2015-01-07 18:06:42 -0600

    added completed column to days table

--- db/tables/days.sql
+++ db/tables/days.sql
@@ -1,6 +1,7 @@
 CREATE TABLE days(
   id serial PRIMARY KEY,
   start date NOT NULL,
+  completed boolean NOT NULL DEFAULT false,
   workout_id integer NOT NULL REFERENCES workouts(id),
   phase_id integer NOT NULL REFERENCES phases(id),
   user_id integer NOT NULL REFERENCES users(id)
