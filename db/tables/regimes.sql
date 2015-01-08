d734a6e73c19d3cba3b0b0b26c4a49b42f6f5c50
Adam Barnhard <admbarn@gmail.com>
2015-01-08 11:16:54 -0600

    fixed unique constraint for workouts to be unique for each user

--- db/tables/regimes.sql
+++ db/tables/regimes.sql
@@ -1,5 +1,6 @@
 CREATE TABLE regimes(
   id serial PRIMARY KEY,
-  name varchar(255) UNIQUE NOT NULL,
-  user_id integer NOT NULL REFERENCES users(id)
+  name varchar(255) NOT NULL,
+  user_id integer NOT NULL REFERENCES users(id),
+  UNIQUE (name, user_id)
 );
